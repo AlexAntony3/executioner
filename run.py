07b4170fe9b0a4d6fdf07416f0b96a2ca6b35d63
AlexAntony3 <alex.antony.jy@gmail.com>
2023-03-05 19:32:08 +0000

    feat: add instructions and adjust message output to make the game more user friendly

--- run.py
+++ run.py
@@ -11,9 +11,10 @@ def difficulty_level(username):
     """
     function to set difficulty level of questions
     """
-    difficulty = input(f'Now {username} you ready to challenge yourself? Pick '
-                       'a difficulty:\nType E for Easy, M for Medium or H for'
-                       ' Hard:\n')
+    difficulty = input(f"Eurgh {username}, what a disgusting name. I know you "
+                       "are never going to win. You can try though, so pick "
+                       f"a difficulty:\n\nType E for Easy, M for Medium or H "
+                       "for Hard:\n")
     if difficulty.upper() == "E":
         word = random.choice(easy_words)
         print(word.upper())
@@ -24,20 +25,21 @@ def difficulty_level(username):
         word = random.choice(hard_words)
         print(word.upper())
     else:
-        raise TypeError("Sorry, you have not entered a valid difficulty")
+        raise TypeError("clearly, that was too many instructions for you, "
+                        "enter a valid letter")
 
 
 def username_prompt():
     """
     function to allow user to enter a username
     """
-    username = input("what would you like to be called?\n")
+    username = input("\nNow, lets get familiar, what should I call you?\n\n")
     return username
 
 
-def main():
+def introduction():
     """
-    main function that runs all other functions
+    Function to call all aspects of the introduction function
     """
     print("""
         ███████╗██╗░░██╗███████╗░█████╗░██╗░░░██╗████████╗██╗░█████╗░███╗░░██╗███████╗██████╗░
@@ -49,6 +51,21 @@ def main():
     """)
     print("Welcome to Executioner! A history based hangman game that has 3 "
           "difficulties, be warned though, they are really tough. \n")
+    print("Here are the instructions:\n"
+          "1. Enter a username\n"
+          "2. Pick the difficulty you would like\n"
+          "3. You are given 6 lives before the man is hanged\n"
+          "4. Guess a letter, if it replaces an _ then you guessed right!\n"
+          "5. However, if you guess incorrectly, you lose a life and the man "
+          "is prepared to be hanged\n"
+          "6. If you guess incorrectly 6 times then the man is EXECUTED!")
+
+
+def main():
+    """
+    main function that runs all other functions
+    """
+    introduction()
     user = username_prompt()
     difficulty_level(user)
 
